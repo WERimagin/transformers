8958b1777381f9bf143c8cb203dac355cdeef86e
笹沢裕一 <werimagin@gmail.com>
2020-10-11 00:16:39 +0900

    add

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -56,12 +56,15 @@ def generate_summaries_or_translations(
         examples_chunk = [prefix + text for text in examples_chunk]
         batch = tokenizer(examples_chunk, return_tensors="pt", truncation=True, padding="longest").to(device)
         print("batch")
+        """
         summaries = model.generate(
             input_ids=batch.input_ids,
-            test=3,
             attention_mask=batch.attention_mask,
             **generate_kwargs,
         )
+        """
+
+        summaries = model.generate()
         dec = tokenizer.batch_decode(summaries, skip_special_tokens=True, clean_up_tokenization_spaces=False)
         for hypothesis in dec:
             fout.write(hypothesis + "\n")
